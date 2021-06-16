04b16263b925cb5576f09f934e232e7076db745c
Robert Zondervan <Robert@conduction.nl>
2021-06-16 15:51:27 +0200

    Remove varnish container, Symfony update, Nginx update

--- api/docker/varnish/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/docker-entrypoint
-
-	# Installing envsubst"
-	echo "Installing envsubst"
-	apk --no-cache add gettext
-
-	# Inserting variables
-	echo "Inserting variables"
-	envsubst < /usr/local/etc/varnish/default.vcl.template > /usr/local/etc/varnish/default.vcl
