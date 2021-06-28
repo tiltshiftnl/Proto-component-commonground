3ede4921052d9a2770a010c124af2c3bbd791427
Barry Brands <barrybrands02@hotmail.com>
2021-06-28 12:17:03 +0200

    Gatsby pages removed unusesd img imports

--- src/components/Logos/index.js
+++ src/components/Logos/index.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import { StyledContainer } from './index.styles.js'
 
-
-import dimpact from './images/dimpact.png'
-import maykin from './images/maykinmedia.png'
 import conduction from './images/conduction.png'
 
 
-import foundationForPublicCode from './images/foundation-for-public-code.png'
-import kadaster from './images/kadaster.png'
+
 
 const Logos = () => (
   <StyledContainer>
